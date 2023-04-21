52d7c80de34e79679424b9e8e0ad657d2cfe400e
gcgbarbosa <gcgbarbosa@gmail.com>
2023-04-20 19:36:23 -0700

    Validate distance with google earth

--- distance.py
+++ distance.py
@@ -17,9 +17,27 @@ def calculate_distance(lat1, lon1, lat2, lon2):
 
     return c * earth_radius_km
 
+#0,Sao-Paulo,-23.559350,-46.557583
+# 1,P1,51.813583,27.431797
 # testing points
-point1 = (0, 1) # geographic coordinates of point 1
-point2 = (1, 1) # geographic coordinates of point 2
+point1 = (-23.559350,-46.557583)
+point2 = (51.813583,27.431797)
+
+#2,San-Jose,9.934261,-84.079025
+#3,P2,-1.514283,14.613158
+point1 = (9.934261,-84.079025)
+point2 = (-1.514283,14.613158)
+
+#4,Montreal,45.501886,-73.567392
+#5,P3,-53.788989,-73.571008
+
+point1 = (45.501886,-73.567392)
+point2 = (-53.788989,-73.571008)
+
+#6,Victoria,48.421769,-123.362389
+#7,P4,-39.989778,-72.517383
+point1 = (48.421769,-123.362389)
+point2 = (-39.989778,-72.517383)
 
 distance = calculate_distance(point1[0], point1[1], point2[0], point2[1])
 
